01ae54a8ed4663e5195d8bb85807454319adc974
iliya-askari <elyaaskarii86@gmail.com>
2024-07-28 19:44:11 +0430

    use base index static files for test

--- core/home/views.py
+++ core/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'home:index.html'
+    template_name = 'home/index.html'
