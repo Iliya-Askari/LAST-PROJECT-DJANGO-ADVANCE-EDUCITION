28832ab70dbb18fd79a1d2d82ca2981f1bb0c202
iliya-askari <elyaaskarii86@gmail.com>
2024-07-29 16:42:45 +0430

    set static file app home

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import *
 
 app_name = 'home'
 urlpatterns = [
-    path("", IndexView.as_view(),name="index")
+    path("", IndexView.as_view(),name="index"),
+    path("about/", AboutView.as_view(),name="about"),
+    path("contact/", ContactView.as_view(),name="contact")
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -3,3 +3,10 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'home/index.html'
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+    
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
+    
