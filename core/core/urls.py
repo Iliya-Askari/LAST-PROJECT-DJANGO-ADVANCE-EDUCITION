5449e7dd4cc63420ff10e4fbf2d78d295f67e4e1
iliya-askari <elyaaskarii86@gmail.com>
2024-07-29 17:29:29 +0430

    complate static services

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('home.urls')),
-    path("blog/",include('blog.urls'))
+    path("blog/",include('blog.urls')),
+    path("services/",include('services.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
