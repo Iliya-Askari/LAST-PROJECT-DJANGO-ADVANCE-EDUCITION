7994fcc2746cf23b3a48005b20f7895db01b2503
iliya-askari <elyaaskarii86@gmail.com>
2024-07-30 14:25:48 +0430

    create coustoum user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,94 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+    Group,
+    Permission,
+)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self , email , password , **extra_fields):
+        if not email:
+            raise (_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email , password , **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password. and extera data
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        # extra_fields.setdefault("is_verified", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser , PermissionsMixin):
+    
+    email = models.EmailField(max_length=255 ,  unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    # is_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
+    
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # related_name سفارشی برای جلوگیری از تداخل
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # related_name سفارشی برای جلوگیری از تداخل
+        blank=True,
+        help_text="Specific permissions for this user.",
+        related_query_name="user",
+    )
+    def __str__(self):
+        return self.email
+    
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, created, instance, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
--- core/core/settings.py
+++ core/core/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'home'
+    'home',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.User'
