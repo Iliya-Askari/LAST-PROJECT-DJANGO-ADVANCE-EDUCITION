2407bd6fdf1c4f3f036626327f7b34209055bd20
IliyaAskari <elyaaskarii86@gmail.com>
2024-08-06 00:17:54 +0330

    add djuser for v1 accounts

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path , include
 from .views import CustomLoginView , CustomSignUpView
 from django.contrib.auth.views import LogoutView
 
@@ -6,5 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', CustomLoginView.as_view(next_page="/"), name='login'),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
-    path('signup/',CustomSignUpView.as_view(),name="register")
+    path('signup/',CustomSignUpView.as_view(),name="register"),
+    path("api/v1/", include("djoser.urls")),
+    path("api/v1/", include("djoser.urls.jwt")),
 ]
